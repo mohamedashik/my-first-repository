a7ffccf5eb033fcae0239f85e35542847a610811
Mohamed Ashik <ashik@insideglobe.com>
2016-05-14 12:22:53 +0530

    first commit

---
+++ app/src/main/java/insideglobe/gittesting/MainActivity.java
@@ -0,0 +1,23 @@
+package insideglobe.gittesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView testGit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testGit=(TextView) findViewById(R.id.testGit);
+
+
+        if (testGit != null) {
+            testGit.setText(R.string.abc);
+        }
+
+
+    }
+}
